72792e3c6b9a4f5ea39ca4731d534775af3f0126
Fiorenzo <40065708+SerFio00@users.noreply.github.com>
2021-02-17 00:16:56 +0100

    aggiunte ricette

--- Items/BuffSword.cs
+++ Items/BuffSword.cs
@@ -15,34 +15,53 @@ namespace Testspada.Items
 
 		public override void SetDefaults() 
 		{
-			item.damage = 20;
+			item.damage = 16;
+			item.crit = 5;
 			item.melee = true;
-			item.width = 100;
-			item.height = 100;
-			item.useTime = 30;
-			item.useAnimation = 20;
+			item.width = 150;
+			item.height = 150;
+			item.useTime = 25;
+			item.useAnimation = 15;
 			item.useStyle = 1;
-			item.knockBack = 6;
+			item.knockBack = 10;
 			item.value = 10000;
-			item.rare = 2;
+			item.rare = 5;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
 		}
 
 		public override void AddRecipes() 
 		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DirtBlock, 1);
-			recipe.AddIngredient(ItemID.Wood, 1);
-			recipe.anyWood = true;
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+			ModRecipe recipeplatinum = new ModRecipe(mod);
+			recipeplatinum.AddIngredient(ItemID.PlatinumBroadsword, 1);
+			recipeplatinum.AddIngredient(ItemID.Daybloom, 5);
+			recipeplatinum.AddIngredient(ItemID.Moonglow, 5);
+			recipeplatinum.AddIngredient(ItemID.Blinkroot, 5);
+			recipeplatinum.AddIngredient(ItemID.Deathweed, 5);
+			recipeplatinum.AddIngredient(ItemID.Waterleaf, 5);
+			recipeplatinum.AddIngredient(ItemID.Fireblossom, 5);
+			recipeplatinum.AddTile(TileID.AlchemyTable);
+			recipeplatinum.AddTile(TileID.Bottles);
+			recipeplatinum.SetResult(this);
+			recipeplatinum.AddRecipe();
+
+			ModRecipe recipegold = new ModRecipe(mod);
+			recipegold.AddIngredient(ItemID.GoldBroadsword, 1);
+			recipegold.AddIngredient(ItemID.Daybloom, 5);
+			recipegold.AddIngredient(ItemID.Moonglow, 5);
+			recipegold.AddIngredient(ItemID.Blinkroot, 5);
+			recipegold.AddIngredient(ItemID.Deathweed, 5);
+			recipegold.AddIngredient(ItemID.Waterleaf, 5);
+			recipegold.AddIngredient(ItemID.Fireblossom, 5);
+			recipegold.AddTile(TileID.AlchemyTable);
+			recipegold.AddTile(TileID.Bottles);
+			recipegold.SetResult(this);
+			recipegold.AddRecipe();
 		}
 
 		public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
         {
-			int[] Potenziamento = { BuffID.AmmoReservation, BuffID.Archery, BuffID.Battle, BuffID.Builder };
+			int[] Potenziamento = { BuffID.AmmoReservation, BuffID.Archery, BuffID.Battle, BuffID.Builder, BuffID.Endurance, BuffID.Flipper, BuffID.Gills, BuffID.Gravitation, BuffID.Heartreach, BuffID.Hunter, BuffID.Inferno, BuffID.Invisibility, BuffID.Ironskin, BuffID.Lifeforce, BuffID.MagicPower, BuffID.ManaRegeneration, BuffID.Mining, BuffID.NightOwl, BuffID.ObsidianSkin, BuffID.Panic, BuffID.Rage, BuffID.RapidHealing, BuffID.Regeneration, BuffID.Shine, BuffID.Sonar, BuffID.Spelunker, BuffID.Swiftness, BuffID.Thorns, BuffID.Titan, BuffID.Warmth, BuffID.WaterWalking, BuffID.WellFed, BuffID.Wrath };
             int Lunghezza = Potenziamento.Length;
 			Random rnd = new Random();
             if (crit)
