d16051e53d26948934603ab157dbc808fff0cbba
Fiorenzo <40065708+SerFio00@users.noreply.github.com>
2021-10-22 11:53:29 +0200

    added custom projectile

--- Items/BuffSword.cs
+++ Items/BuffSword.cs
@@ -29,7 +29,7 @@ namespace Testspada.Items
 			item.rare = ItemRarityID.Pink;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
-			item.shoot = 507;
+			item.shoot = ModContent.ProjectileType<Projectiles.BuffSwordSpear>();
 			item.shootSpeed = 8f;
 		}
 
