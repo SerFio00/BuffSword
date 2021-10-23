91fef1f413863cd36bffb46f4233a218e7a323fd
Fiorenzo <40065708+SerFio00@users.noreply.github.com>
2021-10-23 14:24:47 +0200

    fix

--- Projectiles/BufferSwordFireball.cs
+++ Projectiles/BufferSwordFireball.cs
@@ -36,7 +36,7 @@ namespace Testspada.Projectiles
 
         public override void Kill(int timeLeft)
         {
-            Main.PlaySound(SoundID.explosion, (int)projectile.position.X, (int)projectile.position.Y); // Play a death sound
+            Main.PlaySound(SoundID.14, (int)projectile.position.X, (int)projectile.position.Y); // Play a death sound
             Vector2 usePos = projectile.position; // Position to use for dusts
                                                   // Declaring a constant in-line is fine as it will be optimized by the compiler
                                                   // It is however recommended to define it outside method scope if used elswhere as well
