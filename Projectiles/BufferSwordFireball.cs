2a6ec091e4c06e81915980d769f141de4331dc94
Fiorenzo <40065708+SerFio00@users.noreply.github.com>
2021-10-24 14:47:03 +0200

    fix

--- Projectiles/BufferSwordFireball.cs
+++ Projectiles/BufferSwordFireball.cs
@@ -25,14 +25,14 @@ namespace Testspada.Projectiles
             projectile.friendly = true;         //Can the projectile deal damage to enemies?
             projectile.hostile = false;         //Can the projectile deal damage to the player?
             projectile.ranged = true;           //Is the projectile shoot by a ranged weapon?
-            projectile.penetrate = 2;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
+            projectile.penetrate = 1;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
             projectile.timeLeft = 600;          //The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
             projectile.alpha = 1;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in) Make sure to delete this if you aren't using an aiStyle that fades in. You'll wonder why your projectile is invisible.
             projectile.light = 0f;            //How much light emit around the projectile
             projectile.ignoreWater = true;          //Does the projectile's speed be influenced by water?
             projectile.tileCollide = true;          //Can the projectile collide with tiles?
             projectile.extraUpdates = 1;            //Set to above 0 if you want the projectile to update multiple time in a frame
-            aiType = ProjectileID.ApprenticeStaffT3Shot;           //Act exactly like default Bullet
+            aiType = ProjectileID.DD2BetsyFireball;           //Act exactly like default Bullet
         }
 
         public override void Kill(int timeLeft)
