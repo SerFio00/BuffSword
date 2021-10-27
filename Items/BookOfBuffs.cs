7a729a6354e4aa729c9f706ac439129fb0543b82
Fiorenzo <40065708+SerFio00@users.noreply.github.com>
2021-10-27 19:58:01 +0200

    fix

--- Items/BookOfBuffs.cs
+++ Items/BookOfBuffs.cs
@@ -29,7 +29,7 @@ namespace Testspada.Items
 			item.useAnimation = 16;
 			item.shoot = ModContent.ProjectileType<Projectiles.BookOfBuffsStar>();
 			item.value = Item.sellPrice(silver: 80);
-			Main.PlaySound(2, (int)p.position.X, (int)p.position.Y, 28);
+			Main.PlaySound(2, (int)position.X, (int)position.Y, 28);
 		}
 
 		public override void AddRecipes() {
