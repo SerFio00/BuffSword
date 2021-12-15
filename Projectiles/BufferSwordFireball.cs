442fe784e0fa5f5ff8957cb272f9e79b96ed662b
Fiorenzo <40065708+SerFio00@users.noreply.github.com>
2021-12-15 18:24:30 +0100

    fix

--- Projectiles/BufferSwordFireball.cs
+++ Projectiles/BufferSwordFireball.cs
@@ -54,7 +54,7 @@ namespace Testspada.Projectiles
             }
         }
 
-        public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
+        public virtual void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
 		{
 			//mettere pi� buffs personalizzati e potenziati
 			int[] Potenziamento = { ModContent.BuffType<Buffs.SuperWrath>(), ModContent.BuffType<Buffs.TerriblyPissedOff>(), ModContent.BuffType<Buffs.Fortification>(), ModContent.BuffType<Buffs.BouncyBouncy>(), ModContent.BuffType<Buffs.UltraSwiftness>(), ModContent.BuffType<Buffs.ManaRevenge>(), ModContent.BuffType<Buffs.MageBlessing>(), ModContent.BuffType<Buffs.Regenerator>(), ModContent.BuffType<Buffs.MinionRampage>(), ModContent.BuffType<Buffs.TurretMadness>(), ModContent.BuffType<Buffs.ArmoredPersonnelCarrier>() };
